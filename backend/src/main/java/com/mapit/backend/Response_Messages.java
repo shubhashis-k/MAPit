27f499f5583138b38a8d126806ffbe9f44c2ebf1
shubhashis karmakar <shubhashis@gmail.com>
2015-01-03 01:20:38 +0600

    Backend Integration stage 1, initial integration success

---
+++ backend/src/main/java/com/mapit/backend/Response_Messages.java
@@ -0,0 +1,21 @@
+package com.mapit.backend;
+
+/**
+ * Created by shubhashis on 1/3/2015.
+ */
+public class Response_Messages {
+    public String response_message;
+
+    public final String Userinfo_creation_OK = "OK";
+    public final String Userinfo_creation_duplicate = "DUPLICATE";
+
+    public void setMessage(String message)
+    {
+        response_message = message;
+    }
+
+    public String getMessage()
+    {
+        return response_message;
+    }
+}
---
+++ backend/src/main/java/com/mapit/backend/Userinfo_Model.java
@@ -0,0 +1,50 @@
+package com.mapit.backend;
+
+/**
+ * Created by shubhashis on 1/3/2015.
+ */
+public class Userinfo_Model {
+    private String name, mail, password;
+    private String mobilephone;
+    private String imagedata;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobilephone() {
+        return mobilephone;
+    }
+
+    public void setMobilephone(String mobilephone) {
+        this.mobilephone = mobilephone;
+    }
+
+    public String getImagedata() {
+        return imagedata;
+    }
+
+    public void setImagedata(String imagedata) {
+        this.imagedata = imagedata;
+    }
+}
