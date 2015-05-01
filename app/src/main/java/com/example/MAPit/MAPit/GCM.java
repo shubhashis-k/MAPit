2dbca30b2e4ed56bd874d1cbe6c7bd3aad70d848
shubhashis karmakar <shubhashis@gmail.com>
2015-05-01 22:51:33 +0600

    GCM registration successful

--- app/src/main/java/com/example/MAPit/MAPit/GCM.java
+++ app/src/main/java/com/example/MAPit/MAPit/GCM.java
@@ -12,6 +12,8 @@ public class GCM extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gcm);
+
+        new GcmRegistrationAsyncTask(this).execute();
     }
 
 
