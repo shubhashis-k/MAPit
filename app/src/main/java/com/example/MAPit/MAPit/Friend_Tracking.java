04fcb185776df7391199733101c689a6d6f1345b
setu <setu.1421@yahoo.com>
2015-08-31 13:22:00 +0600

    final version before presentation

--- app/src/main/java/com/example/MAPit/MAPit/Friend_Tracking.java
+++ app/src/main/java/com/example/MAPit/MAPit/Friend_Tracking.java
@@ -21,6 +21,7 @@ import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,6 +29,10 @@ import com.mapit.backend.locationServiceApi.model.LocationService;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
+
+import android.os.Handler;
+import android.widget.Toast;
 
 /**
  * Created by SETU on 5/20/2015.
@@ -41,6 +46,7 @@ public class Friend_Tracking extends Fragment {
     GoogleMap map;
     Double lat, lng;
     RelativeLayout rel;
+    Runnable trackerRunnable;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -73,7 +79,7 @@ public class Friend_Tracking extends Fragment {
             checkForPosition(mail);
             return v;
         }*/
-       while(true) {
+       /*while(true) {
            new Thread(new Runnable() {
                @Override
                public void run() {
@@ -85,84 +91,108 @@ public class Friend_Tracking extends Fragment {
 
 
 
-           }).start();
-           return v;
+           }).start();*/
+        final Handler trackerHandler;
+        trackerHandler = new Handler();
+        trackerHandler.postDelayed(trackerRunnable, 1000);
 
-       }
+        trackerRunnable = new Runnable() {
+            @Override
+            public void run() {
+                checkForPosition(mail);
+                trackerHandler.postDelayed(trackerRunnable, 1000);
+            }
+        };
 
-
+        return v;
 
     }
 
+
     private void checkForPosition(final String mail) {
+        if (map != null) {
+            map.clear();
+        }
+        Log.i("tracking", "i am in while loop");
+        String usermail = mail;
+        LocationService ls = new LocationService();
+        final BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.red_marker);
+        Data d = new Data();
+        d.setUsermail(usermail);
+        d.setCommand(Commands.locService_getInfo.getCommand());
 
-
-                Log.i("tracking", "i am in while loop");
-                String usermail = mail;
-                LocationService ls = new LocationService();
-
-                Data d = new Data();
-                d.setUsermail(usermail);
-                d.setCommand(Commands.locService_getInfo.getCommand());
-
-                try {
-                    new locServiceEndpointCommunicator() {
-                        @Override
-                        protected void onPostExecute(LocationService fetchedData) {
-                            String state = "0";
-                            String last_time = "Unknown";
-                            try {
-                                state = fetchedData.getStatus();
-
-                                DateConverter dc = new DateConverter();
-                                ArrayList<String> formatted = dc.MobileFriendly(fetchedData.getDate());
-
-                                last_time = formatted.get(0) + " " + formatted.get(1);
-
-                                lat = Double.parseDouble(fetchedData.getLatitude());
-                                lng = Double.parseDouble(fetchedData.getLongitude());
-                                if (state.equals("1")) {
-                                    tv_last_seen.setVisibility(View.GONE);
-                                    lastseen.setVisibility(View.GONE);
-                                    MarkerOptions marker = new MarkerOptions().position(new LatLng(lat, lng));
-                                    marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.red_marker));
-                                    map.addMarker(marker);
-                                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 15));
-                                } else {
-                                    online.setText("Offline");
-                                    online.setTextColor(Color.WHITE);
-                                    lastseen.setTextColor(Color.WHITE);
-                                    lastseen.setText(last_time);
-                                    MarkerOptions marker = new MarkerOptions().position(new LatLng(lat, lng));
-                                    marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.red_marker));
-                                    map.addMarker(marker);
-                                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 15));
-
-                                }
-                            } catch (Exception e) {
-                                online.setText("Offline");
-                                online.setTextColor(Color.WHITE);
-                                tv_last_seen.setVisibility(View.GONE);
-                                lastseen.setVisibility(View.GONE);
-
+        try {
+            new locServiceEndpointCommunicator() {
+                @Override
+                protected void onPostExecute(LocationService fetchedData) {
+                    String state = "0";
+                    String last_time = "Unknown";
+                    try {
+                        state = fetchedData.getStatus();
+                        DateConverter dc = new DateConverter();
+                        ArrayList<String> formatted = dc.MobileFriendly(fetchedData.getDate());
+                        last_time = formatted.get(0) + " " + formatted.get(1);
+                        lat = Double.parseDouble(fetchedData.getLatitude());
+                        lng = Double.parseDouble(fetchedData.getLongitude());
+                        if (state.equals("1")) {
+                            tv_last_seen.setVisibility(View.GONE);
+                            lastseen.setVisibility(View.GONE);
+                            online.setText("Online");
+                            online.setTextColor(Color.GREEN);
+                            MarkerOptions marker = new MarkerOptions().position(new LatLng(lat, lng));
+                            //marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.red_marker));
+                            marker.icon(icon);
+                            if (map == null) {
+                                Log.i("tracking", "map is null");
+                                map = mapFrag.getMap();
                             }
+                            map.addMarker(marker);
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 15));
+                        } else {
+                            online.setText("Offline");
+                            online.setTextColor(Color.WHITE);
+                            lastseen.setTextColor(Color.WHITE);
+                            lastseen.setText(last_time);
 
+                            MarkerOptions marker = new MarkerOptions().position(new LatLng(lat, lng));
+                            //marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.red_marker));
+                            marker.icon(icon);
+                            if (map == null) {
+                                Log.i("tracking", "map is null");
+                                map = mapFrag.getMap();
+                            }
+                            map.addMarker(marker);
+
+
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 15));
 
                         }
 
-
+                    } catch (Exception e) {
+                        online.setText("Offline");
+                        online.setTextColor(Color.WHITE);
+                        tv_last_seen.setVisibility(View.GONE);
+                        lastseen.setVisibility(View.GONE);
+                        Toast.makeText(getActivity(), "Internet Connection Problem", Toast.LENGTH_LONG).show();
                     }
-                            .execute(new Pair<Data, LocationService>(d, ls));
-                } catch (Exception e) {
-                    online.setText("Offline");
-                    tv_last_seen.setVisibility(View.GONE);
-                    lastseen.setVisibility(View.GONE);
-
                 }
+            }
 
 
+                    .execute(new Pair<Data, LocationService>(d, ls));
 
+
+        }catch (Exception e){
+            online.setText("Offline");
+            online.setTextColor(Color.WHITE);
+            tv_last_seen.setVisibility(View.GONE);
+            lastseen.setVisibility(View.GONE);
+            Toast.makeText(getActivity(), "Internet Connection Problem", Toast.LENGTH_LONG).show();
+        }
     }
+
+
+
 
     @Override
     public void onResume() {
